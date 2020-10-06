4bbe96fa598100a0de6903df82bfb3e57368a88d
scottpwells <scottpwells@gmail.com>
2020-10-06 14:28:17 -0400

    added time blocks and jquery framework

---
+++ script.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+
+// dom variables
+var body = $("<body>");
+var scheduleData = $("<textarea>");
+
+
+
+//js variables
+
+
+var currentDay = $("#currentDay");
+var description = $(".description")
+var timeBlock = $(".time-block");
+var hour = $(".hour");
+var past = $(".past");
+var present = $(".present");
+var future = $(".future");
+var saveBtn = $(".saveBtn");
+
+
+
+
+
+//function calls
+
+$("currentDay")
+
+
+
+
+
+
+
+//array of time-block colors
+var colors = [".past",".present",".future"];
+
+
+//Function Definitions
+
+ $(".time-block").on("click", function(){
+    for(i = 0; i < colors.length; i++)
+ ]  if 
+    
+    });
+
+
+
+
+//Event Listeners
+// listen to all the time blocks
+// use event delegation to pull the value
+// store it for later use
+
+
+
+
+
+
+//listen to the save button
+// store to local storage
+
+
+
+
+
+
+
+
+
+
+});
